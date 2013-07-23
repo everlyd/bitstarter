b1deb7b1b64407772a3f1efb8057a7d2e0119e1f
John Smith <example@stanford.edu>
2013-07-23 06:29:54 +0000

    Read from index.html

--- web.js
+++ web.js
@@ -2,11 +2,19 @@ var express = require('express');
 var app = express();
 app.use(express.logger());
 
+
 app.get('/', function(request, response) {
-  response.send('Hello World2!');
+//  response.send('Hello World2!');
+var fs = require('fs');
+var content=fs.readFileSync('index.html');
+response.send(content.toString());
 });
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
+
+
+//fs.readFileSync
+//Buffer.toString
